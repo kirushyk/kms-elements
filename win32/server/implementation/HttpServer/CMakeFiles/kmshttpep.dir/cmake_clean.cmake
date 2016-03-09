cbb929cc51925091c1053b73208e9aae4d94dc14
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-03-09 02:22:05 +0200

    mingw32: Add generated sources for implementation

---
+++ win32/server/implementation/HttpServer/CMakeFiles/kmshttpep.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "http-marshal.c"
+  "http-enumtypes.c"
+  "http-marshal.h"
+  "http-enumtypes.h"
+  "CMakeFiles/kmshttpep.dir/KmsHttpEPServer.cpp.o"
+  "CMakeFiles/kmshttpep.dir/KmsHttpPost.cpp.o"
+  "CMakeFiles/kmshttpep.dir/HttpEndPointServer.cpp.o"
+  "CMakeFiles/kmshttpep.dir/http-marshal.c.o"
+  "CMakeFiles/kmshttpep.dir/http-enumtypes.c.o"
+  "libkmshttpep.pdb"
+  "libkmshttpep.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/kmshttpep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ win32/server/implementation/HttpServer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ky/kms-elements/src/server/implementation/HttpServer
+# Build directory: /home/ky/kms-elements/build/src/server/implementation/HttpServer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
